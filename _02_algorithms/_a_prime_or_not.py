a032991487d13ce23426aea42b3c72b8053c4382
league <bobby1545>
2025-06-21 11:29:18 -0700

    hahaha

--- _02_algorithms/_a_prime_or_not.py
+++ _02_algorithms/_a_prime_or_not.py
@@ -3,13 +3,22 @@ Write code that can tell if a number is prime.
 A prime number is a number that is only divisible by 1 and itself.
 """
 from tkinter import messagebox, simpledialog, Tk
-
+root = Tk()
+root.withdraw()
 
 if __name__ == '__main__':
     # TODO)
-    #  1. Ask the user for a number
-    #  2. Use a for loop, if statement, and modulo to find if the number
+    #  1. Ask the user for a number.
+    number = simpledialog.askinteger(title="b", prompt="Type a number between 2 and a trillion.")
+    for i in range(2, number):
+        if number % i == 0:
+            messagebox.showinfo(title="d", message=f"{number} is not prime because it is divisible by {i}.")
+            quit()
+    messagebox.showinfo(title="d", message=f"{number} is prime.")
+
+    #  2. Use a for loop, if statement, and mod
+    #         print(i)ulo to find if the number
     #     is prime.
+
     #  3. If the number is divisible by any number other than 1 or itself,
     #     the number is not prime.
-    pass

68d9ce7e4044c567b71276e892ab7e32729937d6
Aarit Jayaswal <aaritrj@gmail.com>
2025-07-30 19:58:27 -0700

    HEHEHEHEHEHEHEHEHHEHEHE

--- _01_function_writing/_e_houses.py
+++ _01_function_writing/_e_houses.py
@@ -10,10 +10,9 @@ if __name__ == '__main__':
     turtle = turtle.Turtle()
     #   2) Draw ONE flat-topped house with height=100 and green grass after it.
     turtle.left(90)
-    turtle.forward(15)
+    turtle.forward(150)
     turtle.right(90)
-    turtle.forward(10)
-    turtle.forward(15)
+    turtle.forward(100)
     #   3) Put the code that drew the house into a function called draw_house
     #      HINT: Only the code that draws one house should go in this function.
     #   4) Using the function you just created, draw 10 houses.
@@ -31,4 +30,4 @@ if __name__ == '__main__':
     #      (JUST the roof part): draw_pointy_roof, draw_flat_roof
     #   11) By calling the correct "roof" function, make large houses have
     #      flat roofs and all the others have pointy roofs.
-    turtle.done()
+    exit()
